4f0d5ae6403b805ac1264e8dc3158566ed660f0b
Calipocalypse <calipocalypse@gmail.com>
2022-06-30 22:20:44 +0200

    Another colorful table generator

--- Netphonebook.Modules.Models/ViewModels/ModelCreatorViewModel.cs
+++ Netphonebook.Modules.Models/ViewModels/ModelCreatorViewModel.cs
@@ -6,7 +6,9 @@ using Prism.Mvvm;
 using Prism.Regions;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
+using System.Windows.Media;
 
 namespace Netphonebook.Modules.Models.ViewModels
 {
@@ -50,6 +52,8 @@ namespace Netphonebook.Modules.Models.ViewModels
         private void OnCellChange()
         {
             RaisePropertyChanged(nameof(FontSize));
+            RaisePropertyChanged(nameof(BorderSize));
+            RaisePropertyChanged(nameof(CornerRadius));
         }
 
         private sbyte[] fontSize = new sbyte[6];
@@ -59,24 +63,27 @@ namespace Netphonebook.Modules.Models.ViewModels
             set { SetProperty(ref fontSize[SelectedCellNumber-1], value); }
         }
 
-        private sbyte borderSize;
+        private sbyte[] borderSize = new sbyte[6];
         public sbyte BorderSize
         {
-            get { return borderSize; }
-            set { SetProperty(ref borderSize, value); }
+            get { return borderSize[SelectedCellNumber-1]; }
+            set { SetProperty(ref borderSize[SelectedCellNumber-1], value); }
         }
 
-        private sbyte cornerRadius;
+        private sbyte[] cornerRadius = new sbyte[6];
         public sbyte CornerRadius
         {
-            get { return cornerRadius; }
-            set { SetProperty(ref cornerRadius, value); }
+            get { return cornerRadius[SelectedCellNumber-1]; }
+            set { SetProperty(ref cornerRadius[SelectedCellNumber-1], value); }
         }
+
+        public ObservableCollection<SolidColorBrush> ColorList { get; set; }
 
         public ModelCreatorViewModel(IRegionManager regionManager, IDataProvider dataProvider)
         {
             _regionManager = regionManager;
             _dataProvider = dataProvider;
+            ColorList = GetSolidColorBrushes();
             ClickBack = new DelegateCommand(NavigateBack);
             ClickAdd = new DelegateCommand(ClickedAdd);
         }
@@ -107,6 +114,23 @@ namespace Netphonebook.Modules.Models.ViewModels
             _regionManager.Regions["ContentRegion"].RemoveAll();
             _regionManager.Regions["ContentRegion"].Add(new ModulesView(), "ModelsViewer");
             var view = _regionManager.Regions["ContentRegion"].GetView("ModelsViewer");
+        }
+
+        private ObservableCollection<SolidColorBrush> GetSolidColorBrushes()
+        {
+            var brushes = new ObservableCollection<SolidColorBrush>();
+            for (int i = 0; i <= 255; i+=40)
+            {
+                for (int j = 0; j <= 255; j+=40)
+                {
+                    for(int k = 0; k <= 255; k+=40)
+                    {
+                        var color = Color.FromRgb((byte)i, (byte)j, (byte)k);
+                        brushes.Add(new SolidColorBrush(color));
+                    }
+                }
+            }
+            return brushes;
         }
     }
 }
